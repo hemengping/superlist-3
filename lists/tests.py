1fda58e2f222e9f3a50e38dd01745b4e33d318be
hemengping <1113418206@qq.com>
2019-09-10 19:56:02 +0800

    添加测试代码

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,21 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEquals(1+1,3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/home/')
+        self.assertEqual(found.func,home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        print(f"html:{html}")
+        self.assertTrue(html.startswith('H'))
+        self.assertIn('<title>To-Do lists</tittle>',html)
+        self.assertTrue(html.endswith('d'))
+
