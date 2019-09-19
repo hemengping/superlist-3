c417bd53e95c00030078f6b19f0f08aa9ccc9399
hemengping <1113418206@qq.com>
2019-09-19 16:02:26 +0800

    使用Selenium测试用户交互

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -8,10 +10,36 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retriieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://127.0.0.1:8000/')
 
         self.assertIn('To-Do',self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
+        #应用邀请她输入一个待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        #她在文本框输入了'Buy peacock feathers'
+        #伊迪斯的爱好是使用假蝇做鱼饵钓鱼
+        inputbox.send_keys('Buy peacock feathers')
+
+        #他按回车键后，页面更新了
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers' for row in rows)
+        )
+
+        #页面中又显示了一个文本框，可以输入其他待办事项
+        #他输入了“Use peacock feathers to make a fly”
+        #伊迪斯做事很有条理
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
