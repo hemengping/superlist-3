e412719e3c34e1ee747e5c732527daffa7c11a09
hemengping <1113418206@qq.com>
2019-09-19 19:18:50 +0800

    修改首页内容

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -11,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://127.0.0.1:8000/')
+        self.browser.get('http://127.0.0.1:8000/home/')
 
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -32,9 +32,10 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows)
+            any(row.text == '1:Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         #页面中又显示了一个文本框，可以输入其他待办事项
