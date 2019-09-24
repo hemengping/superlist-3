b277858e512b45eba434552fefef4b8067c2ea10
hemengping <1113418206@qq.com>
2019-09-24 19:59:37 +0800

    数据库迁移

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from lists.models import Item
 from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
+    # item = Item()
+    # item.text = request.POST.get('item_text','')
+    # item.save()
+
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+    else:
+        new_item_text = ''
+
     return render(request,'home.html',{
-        'new_item_text':request.POST.get('item_text','')
+        'new_item_text':new_item_text,
     })
