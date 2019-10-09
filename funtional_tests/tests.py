fab929f0f9d86cd2e64bc51aa282f436453d4d7b
hemengping <1113418206@qq.com>
2019-10-09 17:06:17 +0800

    '新增一个functional_tests文件夹'

--- funtional_tests/tests.py
+++ funtional_tests/tests.py
@@ -7,10 +7,12 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
+
     def tearDown(self):
         self.browser.quit()
 
     def check_for_row_in_list_table(self,row_text):
+        time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text,[row.text for row in rows])
--- lists/views.py
+++ lists/views.py
@@ -10,10 +10,11 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/home/')
     else:
         new_item_text = ''
 
+    items = Item.objects.all()
     return render(request,'home.html',{
-        'new_item_text':new_item_text,
+        'items':items,
     })
